81633167a8ee97c0deb28ee5bd6821149fc66560
luis-hens <luishensp@gmail.com>
2022-08-13 22:12:02 -0300

    Fix script

--- lhserver_apache.sh
+++ lhserver_apache.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "Update"
 apt-get -q update -y
 apt-get -q upgrade --y
-echo "Done"
 apt-get -q install apache2 -y
 apt-get -q install unzip -y
 
 wget -P /temp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip /temp/main.zip -d /var/www/html
+mv /var/www/html/linux-site-dio-main/* /var/www/html/
+mv /var/www/html/linux-site-dio-main/.* /var/www/html/
+rmdir /var/www/html/linux-site-dio-main
+
+echo "Done"
